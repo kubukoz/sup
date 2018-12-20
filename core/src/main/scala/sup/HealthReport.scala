60c0d9174aa78e050f9ad70df6409b07f98c8702
Jakub Kozłowski <kubukoz@gmail.com>
2018-12-20 11:10:44 +0100

    Remodel HealthReport

--- core/src/main/scala/sup/HealthReport.scala
+++ core/src/main/scala/sup/HealthReport.scala
@@ -13,19 +13,12 @@ object HealthReporter {
   def fromChecks[F[_]: Applicative](first: HealthCheck[F], rest: HealthCheck[F]*): HealthReporter[F] =
     new HealthReporter[F] {
       override val report: F[HealthReport] = {
-        NonEmptyList(first, rest.toList).traverse(_.check).map {
-          case results if results.forall(_.isGood) => HealthReport.Good(results)
-          case results                             => HealthReport.Bad(results)
+        NonEmptyList(first, rest.toList).traverse(_.check).map { results =>
+          val status = if (results.forall(_.isGood)) Health.Good else Health.Bad
+          HealthReport(results, status)
         }
       }
     }
 }
 
-sealed trait HealthReport extends Product with Serializable {
-  def checks: NonEmptyList[Health]
-}
-
-object HealthReport {
-  case class Good(checks: NonEmptyList[Health]) extends HealthReport
-  case class Bad(checks: NonEmptyList[Health]) extends HealthReport
-}
+case class HealthReport(checks: NonEmptyList[Health], status: Health)
