89c978b99869af9452200022792032e305876728
Jakub Kozłowski <kubukoz@gmail.com>
2018-12-20 11:12:46 +0100

    Split data into files

---
+++ core/src/main/scala/sup/Health.scala
@@ -0,0 +1,13 @@
+package sup
+
+sealed trait Health extends Product with Serializable {
+  def isGood: Boolean = this match {
+    case Health.Good => true
+    case Health.Bad => false
+  }
+}
+
+object Health {
+  case object Good extends Health
+  case object Bad extends Health
+}
--- core/src/main/scala/sup/HealthReport.scala
+++ core/src/main/scala/sup/HealthReport.scala
@@ -1,24 +1,5 @@
 package sup
 
 import cats.data.NonEmptyList
-import cats.Applicative
-import cats.implicits._
-
-trait HealthReporter[F[_]] {
-  def report: F[HealthReport]
-}
-
-object HealthReporter {
-
-  def fromChecks[F[_]: Applicative](first: HealthCheck[F], rest: HealthCheck[F]*): HealthReporter[F] =
-    new HealthReporter[F] {
-      override val report: F[HealthReport] = {
-        NonEmptyList(first, rest.toList).traverse(_.check).map { results =>
-          val status = if (results.forall(_.isGood)) Health.Good else Health.Bad
-          HealthReport(results, status)
-        }
-      }
-    }
-}
 
 case class HealthReport(checks: NonEmptyList[Health], status: Health)
