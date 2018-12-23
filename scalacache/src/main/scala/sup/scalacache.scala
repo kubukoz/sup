8501f0994235f660d401bb794c57100d164f5e80
Jakub Kozłowski <kubukoz@gmail.com>
2018-12-23 15:44:54 +0100

    Make flags customizable

--- scalacache/src/main/scala/sup/scalacache.scala
+++ scalacache/src/main/scala/sup/scalacache.scala
@@ -9,9 +9,9 @@ object scalacache {
   /**
     * Use with [[HealthCheck.transform]].
     * */
-  def cached[F[_], H[_]](key: String, ttl: Option[Duration])(
-    implicit cache: Cache[HealthResult[H]],
-    mode: Mode[F]): Endo[F[HealthResult[H]]] = { action =>
+  def cached[F[_], H[_]](key: String, ttl: Option[Duration])(implicit cache: Cache[HealthResult[H]],
+                                                             mode: Mode[F],
+                                                             flags: Flags): Endo[F[HealthResult[H]]] = { action =>
     cache.cachingForMemoizeF(key)(ttl)(action)
   }
 }
