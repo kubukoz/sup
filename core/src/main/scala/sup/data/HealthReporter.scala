8583dab3d1c95f494c537dd18eb132f2ffe77079
Jakub Kozłowski <kubukoz@gmail.com>
2018-12-25 02:47:58 +0100

    Ensure the Health monoid isn't used anywhere where it's not customizable

--- core/src/main/scala/sup/data/HealthReporter.scala
+++ core/src/main/scala/sup/data/HealthReporter.scala
@@ -3,7 +3,7 @@ package sup.data
 import cats.data.{NonEmptyList, OneAnd}
 import cats.implicits._
 import cats.kernel.Semigroup
-import cats.{Apply, Foldable, NonEmptyTraverse}
+import cats.{Apply, Foldable, NonEmptyTraverse, Reducible}
 import sup._
 
 object HealthReporter {
@@ -11,7 +11,7 @@ object HealthReporter {
   /**
     * Equivalent to [[wrapChecks]] for G = NonEmptyList, with more pleasant syntax.
     * */
-  def fromChecks[F[_]: Apply, H[_]: Foldable](first: HealthCheck[F, H], rest: HealthCheck[F, H]*)(
+  def fromChecks[F[_]: Apply, H[_]: Reducible](first: HealthCheck[F, H], rest: HealthCheck[F, H]*)(
     implicit M: Semigroup[Health]): HealthReporter[F, (NonEmptyList ∘ H)#λ] =
     wrapChecks(NonEmptyList(first, rest.toList))
 
@@ -21,14 +21,14 @@ object HealthReporter {
     *
     * e.g. if all checks need to return Healthy for the whole thing to be healthy, use [[Health.allHealthyCommutativeMonoid]].
     */
-  def wrapChecks[F[_]: Apply, G[_]: NonEmptyTraverse, H[_]: Foldable](checks: G[HealthCheck[F, H]])(
+  def wrapChecks[F[_]: Apply, G[_]: NonEmptyTraverse, H[_]: Reducible](checks: G[HealthCheck[F, H]])(
     implicit M: Semigroup[Health]): HealthReporter[F, (G ∘ H)#λ] = {
     type GH[A] = G[H[A]]
 
     new HealthReporter[F, GH] {
       override val check: F[HealthResult[OneAnd[GH, ?]]] = {
         checks.nonEmptyTraverse(_.check).map { results =>
-          val status = results.reduceMap(_.value.combineAll)
+          val status = results.reduceMap(_.value.reduce)
 
           HealthResult(OneAnd[GH, Health](status, results.map(_.value)))
         }
