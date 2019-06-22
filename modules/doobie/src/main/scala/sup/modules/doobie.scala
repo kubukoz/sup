e8e333f0c76fc9acc28e7d1b18aba0359ad50709
Jakub Kozłowski <kubukoz@gmail.com>
2019-06-22 17:50:48 +0200

    Update code

--- modules/doobie/src/main/scala/sup/modules/doobie.scala
+++ modules/doobie/src/main/scala/sup/modules/doobie.scala
@@ -3,6 +3,7 @@ package sup.modules
 import cats.{Id, Monad}
 import eu.timepit.refined.types.numeric.PosInt
 import sup.HealthCheck
+import cats.effect.Bracket
 
 object doobie {
   import _root_.doobie._
@@ -15,7 +16,7 @@ object doobie {
     * Note: Errors aren't recovered in this healthcheck. If you want error handling,
     * consider using [[HealthCheck.through]] with [[sup.mods.recoverToSick]].
     * */
-  def connectionCheck[F[_]: Monad](xa: Transactor[F])(timeoutSeconds: Option[PosInt]): HealthCheck[F, Id] = {
+  def connectionCheck[F[_]: Bracket[?[_], Throwable]](xa: Transactor[F])(timeoutSeconds: Option[PosInt]): HealthCheck[F, Id] = {
     //zero means infinite in JDBC
     val actualTimeoutSeconds = timeoutSeconds.fold(0)(_.value)
 
