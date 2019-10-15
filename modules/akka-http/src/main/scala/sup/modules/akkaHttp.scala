f3363862b7dffc746c9d878dd793d65ae6ede227
Dennis van der Bij <dennis@swissborg.com>
2019-10-15 20:25:06 +0200

    add routes with context

--- modules/akka-http/src/main/scala/sup/modules/akkaHttp.scala
+++ modules/akka-http/src/main/scala/sup/modules/akkaHttp.scala
@@ -3,13 +3,14 @@ package sup.modules
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import akka.http.scaladsl.model.{StatusCode, StatusCodes}
 import akka.http.scaladsl.server.Directives.{path => akkaPath, _}
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{RequestContext, Route}
 import cats.effect.Effect
 import cats.syntax.functor._
 import cats.syntax.reducible._
-import cats.{Functor, Reducible, Semigroup}
+import cats.{Functor, Reducible, Semigroup, ~>}
 import sup.{Health, HealthCheck, HealthResult}
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 object akkaHttp {
@@ -35,11 +36,26 @@ object akkaHttp {
     }
   }
 
-  def healthCheckResponse[F[_]: Functor, H[_]: Reducible](healthCheck: HealthCheck[F, H])(
+  def healthCheckResponse[F[_] : Functor, H[_] : Reducible](healthCheck: HealthCheck[F, H])(
     implicit combineHealthChecks: Semigroup[Health]): F[(StatusCode, HealthResult[H])] = {
     healthCheck.check.map { check =>
       if (check.value.reduce.isHealthy) StatusCodes.OK -> check
       else StatusCodes.ServiceUnavailable -> check
+    }
+  }
+
+  def healthCheckRoutesWithContext[F[_]: Functor, H[_] : Reducible, R](
+    healthCheck: HealthCheck[F, H],
+    path: String = "health-check",
+    f: RequestContext => F ~> Future)
+  (implicit marshaller: ToEntityMarshaller[HealthResult[H]], combineHealthChecks: Semigroup[Health]): Route = {
+    akkaPath(path) { requestCtx =>
+      get {
+        onComplete(f(requestCtx)(healthCheckResponse(healthCheck))) {
+          case Success(response) => complete(response)
+          case Failure(e) => failWith(e)
+        }
+      }(requestCtx)
     }
   }
 }
