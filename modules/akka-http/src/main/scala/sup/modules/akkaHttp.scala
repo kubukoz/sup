ad0db70c0d44b5282327779a83a77e282cbb3053
Dennis van der Bij <dennis@swissborg.com>
2019-10-15 20:03:30 +0200

    add response only helper

--- modules/akka-http/src/main/scala/sup/modules/akkaHttp.scala
+++ modules/akka-http/src/main/scala/sup/modules/akkaHttp.scala
@@ -1,13 +1,13 @@
 package sup.modules
 
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{StatusCode, StatusCodes}
 import akka.http.scaladsl.server.Directives.{path => akkaPath, _}
 import akka.http.scaladsl.server.Route
-import cats.Reducible
 import cats.effect.Effect
-import cats.Semigroup
+import cats.syntax.functor._
 import cats.syntax.reducible._
+import cats.{Functor, Reducible, Semigroup}
 import sup.{Health, HealthCheck, HealthResult}
 
 import scala.util.{Failure, Success}
@@ -16,7 +16,8 @@ object akkaHttp {
 
   /**
    * Builds a Route value that'll check the result of the healthcheck, and,
-   * if it's sick, return ServiceUnavailable (Ok otherwise).
+   * if it's sick, return ServiceUnavailable (Ok otherwise). See [[healthCheckResponse]]
+   * for an alternative that doesn't provide a route matcher.
    *
    * The semigroup is used only to determine whether the overall health status is healthy.
    **/
@@ -26,15 +27,19 @@ object akkaHttp {
   (implicit marshaller: ToEntityMarshaller[HealthResult[H]], combineHealthChecks: Semigroup[Health]): Route = {
     akkaPath(path) {
       get {
-        onComplete(Effect[F].toIO(healthCheck.check).unsafeToFuture()) {
-          case Success(check) =>
-            if (check.value.reduce.isHealthy) complete(StatusCodes.OK -> check)
-            else complete(StatusCodes.ServiceUnavailable -> check)
-
-          case Failure(e) =>
-            failWith(e)
+        onComplete(Effect[F].toIO(healthCheckResponse(healthCheck)).unsafeToFuture()) {
+          case Success(response) => complete(response)
+          case Failure(e) => failWith(e)
         }
       }
+    }
+  }
+
+  def healthCheckResponse[F[_]: Functor, H[_]: Reducible](healthCheck: HealthCheck[F, H])(
+    implicit combineHealthChecks: Semigroup[Health]): F[(StatusCode, HealthResult[H])] = {
+    healthCheck.check.map { check =>
+      if (check.value.reduce.isHealthy) StatusCodes.OK -> check
+      else StatusCodes.ServiceUnavailable -> check
     }
   }
 }
